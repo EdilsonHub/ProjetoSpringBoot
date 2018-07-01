aa909856c292f06afff57dffcc52906b1488f564
Edilson Pereira da Silva <di3.pereiradasilva@gmail.com>
2018-06-30 23:37:17 -0300

    Testando a primeira classe de domínio - Categoria

--- src/main/java/com/edilson/cursoms/resources/CategoriaResources.java
+++ src/main/java/com/edilson/cursoms/resources/CategoriaResources.java
@@ -1,15 +1,29 @@
 package com.edilson.cursoms.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.edilson.cursoms.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "O REST esta funcionando perfeitamente";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1,"Informática");
+		Categoria categoria2 = new Categoria(1,"Escritório");
+		List<Categoria> lista = new ArrayList<Categoria>();
+		
+		lista.add(categoria1);
+		lista.add(categoria2);
+		
+		
+		return lista;
 	}
 }
