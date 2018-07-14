5fea17936b884148b5cab51c68f5f335087daa51
Edilson Pereira da Silva <di3.pereiradasilva@gmail.com>
2018-07-14 15:24:11 -0300

    Ajustando a deleção de Clientes incluindo endidades associadas (e pequenas correções)

--- src/main/java/com/edilson/cursoms/services/ClienteService.java
+++ src/main/java/com/edilson/cursoms/services/ClienteService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.edilson.cursoms.domain.Cidade;
 import com.edilson.cursoms.domain.Cliente;
@@ -18,7 +19,6 @@ import com.edilson.cursoms.domain.Endereco;
 import com.edilson.cursoms.dtos.ClienteDTO;
 import com.edilson.cursoms.dtos.ClienteNewDTO;
 import com.edilson.cursoms.enums.TipoCliente;
-import com.edilson.cursoms.repositories.CidadeRepository;
 import com.edilson.cursoms.repositories.ClienteRepository;
 import com.edilson.cursoms.repositories.EnderecoRepository;
 import com.edilson.cursoms.services.exceptions.DataIntegrityException;
@@ -29,8 +29,8 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
-	@Autowired
-	private CidadeRepository cidadeRepository;
+//	@Autowired
+//	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 
@@ -61,7 +61,8 @@ public class ClienteService {
 	
 	public Cliente fromDTO(@Valid ClienteNewDTO objDTO) {
 		Cliente cliente = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(),TipoCliente.toEnum(objDTO.getTipo()));
-		Cidade cidade = cidadeRepository.findById(objDTO.getCidadeId()).orElse(null);
+//		Cidade cidade = cidadeRepository.findById(objDTO.getCidadeId()).orElse(null);
+		Cidade cidade = new Cidade(objDTO.getCidadeId(),null,null);
 		Endereco endereco = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cliente, cidade);
 		cliente.getEnderecos().add(endereco);
 		cliente.getTelefones().add(objDTO.getTelefone1());
@@ -73,6 +74,7 @@ public class ClienteService {
 		}
 		return cliente;
 	}
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		Cliente cli = clienteRepository.save(obj);
@@ -86,7 +88,7 @@ public class ClienteService {
 		try {
 			this.clienteRepository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possivel excluir, porque possue entidades associadas!");
+			throw new DataIntegrityException("Não é possivel apagar o registro de um cliente que possue pedidos");
 		}
 	}
 	private void updateFromData(Cliente newObj, Cliente obj) {
